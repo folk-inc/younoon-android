44a68ae762ec46d2fe39408033b1a9b60788d616
ali.sahraei <ali.sahraei110@gmail.com>
2021-01-18 19:13:50 +0330

    test for main activity

--- app/src/main/java/org/folk/younoon/MainActivity.java
+++ app/src/main/java/org/folk/younoon/MainActivity.java
@@ -34,67 +34,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findID();
-        websetting();
-        webView.loadUrl(url);
-        load();
+
+//        ((YounoonApplication) getApplication()).refreshLocale(this);
+
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         initService();
+    	Intent intent;
+    	intent = new Intent(MainActivity.this, WebViewActivity.class);
+
     }
 
-    public void findID(){
-        webView = findViewById(R.id.webview);
-        linearLayout = findViewById(R.id.pgData);
-    }
-
-    public void load(){
-        webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap facIcon) {
-                linearLayout.setVisibility(View.VISIBLE);
-            }
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                linearLayout.setVisibility(View.GONE);
-            }
-        });
-    };
-
-    @SuppressLint({"JavascriptInterface", "SetJavaScriptEnabled"})
-    public void websetting(){
-        WebSettings webSetting = webView.getSettings();
-        webSetting.setJavaScriptEnabled(true);
-        webSetting.setDomStorageEnabled(true);
-        webSetting.setPluginState(WebSettings.PluginState.ON);
-        webSetting.setLoadsImagesAutomatically(true);
-        webSetting.setLoadWithOverviewMode(true);
-        webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        webSetting.setSupportZoom(false);
-        webSetting.setSavePassword(true);
-        webSetting.setBlockNetworkImage(false);
-        webSetting.setSupportMultipleWindows(false);
-        webSetting.setAppCacheEnabled(true);
-        webSetting.setSupportZoom(false);
-        webSetting.setAllowFileAccess(true);
-        webSetting.setAllowContentAccess(true);
-        webSetting.setBuiltInZoomControls(true);
-        webSetting.setUseWideViewPort(true);
-        webView.setScrollbarFadingEnabled(false);
-        webView.setHorizontalScrollBarEnabled(false);
-        webView.setVerticalScrollBarEnabled(false);
-        webView.setScrollContainer(false);
-        webView.addJavascriptInterface(this, "jsinterface");
-        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-    }
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
