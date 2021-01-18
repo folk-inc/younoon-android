caa490bd4331eb0ee05884e61cc3aad219458e9a
ali.sahraei <ali.sahraei110@gmail.com>
2021-01-18 18:18:08 +0330

    web view class

---
+++ app/src/main/java/org/folk/younoon/WebViewActivity.java
@@ -0,0 +1,8 @@
+package org.folk.younoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import im.delight.android.webview.AdvancedWebView;
+
+public class WebViewActivity extends AppCompatActivity implements  AdvancedWebView.Listener{
+}
