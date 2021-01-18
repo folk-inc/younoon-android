84a0a33db75dd710370c1d9c4caa97d5550d5b7a
ali.sahraei <ali.sahraei110@gmail.com>
2021-01-18 18:25:37 +0330

    web view class and web view setting

--- app/src/main/java/org/folk/younoon/WebViewActivity.java
+++ app/src/main/java/org/folk/younoon/WebViewActivity.java
@@ -1,8 +1,87 @@
 package org.folk.younoon;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import im.delight.android.webview.AdvancedWebView;
 
 public class WebViewActivity extends AppCompatActivity implements  AdvancedWebView.Listener{
+    LinearLayout linearLayout;
+    private AdvancedWebView webView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        findId();
+        websetting();
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public void findId(){
+        linearLayout = findViewById(R.id.pgData);
+    }
+
+    @SuppressLint({"JavascriptInterface", "SetJavaScriptEnabled"})
+    public void websetting(){
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setPluginState(WebSettings.PluginState.ON);
+        webSetting.setLoadsImagesAutomatically(true);
+        webSetting.setLoadWithOverviewMode(true);
+        webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSetting.setSupportZoom(false);
+        webSetting.setSavePassword(true);
+        webSetting.setBlockNetworkImage(false);
+        webSetting.setSupportMultipleWindows(false);
+        webSetting.setAppCacheEnabled(true);
+        webSetting.setSupportZoom(false);
+        webSetting.setAllowFileAccess(true);
+        webSetting.setAllowContentAccess(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setUseWideViewPort(true);
+        webView.setScrollbarFadingEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setScrollContainer(false);
+        webView.addJavascriptInterface(this, "jsinterface");
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+
+    }
+
+    @Override
+    public void onExternalPageRequest(String url) {
+
+    }
 }
