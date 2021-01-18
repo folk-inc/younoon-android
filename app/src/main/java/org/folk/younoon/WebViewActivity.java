5dbf993757bfdba06807f40dba351272d376e7de
ali.sahraei <ali.sahraei110@gmail.com>
2021-01-18 18:44:36 +0330

    check network and show bottom sheet

--- app/src/main/java/org/folk/younoon/WebViewActivity.java
+++ app/src/main/java/org/folk/younoon/WebViewActivity.java
@@ -1,13 +1,17 @@
 package org.folk.younoon;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,9 +21,12 @@ import im.delight.android.webview.AdvancedWebView;
 public class WebViewActivity extends AppCompatActivity implements  AdvancedWebView.Listener{
     LinearLayout linearLayout;
     private AdvancedWebView webView;
+    String TAG = "folk-inc";
     String url = "https://younoon.ir/";
     boolean doubleBackToExitPressedOnce = false;
     boolean goToIntro = false;
+    private ProgressBar progress;
+    boolean bottomSheetIsShow = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -89,19 +96,57 @@ public class WebViewActivity extends AppCompatActivity implements  AdvancedWebVi
         });
     };
 
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+        }
+        return false;
+    }
+
+    public void showBottomSheetDialogFragment() {
+        if (!bottomSheetIsShow){
+            progress.setVisibility(View.GONE);
+            bottomSheetIsShow = true;
+            webView.stopLoading();
+            webView.setVisibility(View.GONE);
+            BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+            bottomSheetFragment.setCancelable(false);
+            bottomSheetFragment.setListener(() -> {
+                bottomSheetIsShow = false;
+                webView.reload();
+                bottomSheetFragment.dismiss();
+            });
+            bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+        }
+    }
+
     @Override
     public void onPageStarted(String url, Bitmap favicon) {
-
+        progress.setVisibility(View.VISIBLE);
+        if (!isNetworkConnected()) showBottomSheetDialogFragment();
+        Log.d(TAG, "onPageStarted: "+url);
     }
 
     @Override
     public void onPageFinished(String url) {
-
+        Log.d(TAG, "onPageFinished: "+url);
+        progress.setVisibility(View.GONE);
+        if (isNetworkConnected())
+            if (webView.getVisibility() != View.VISIBLE)
+                webView.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onPageError(int errorCode, String description, String failingUrl) {
-
+        if (description.equals("net::ERR_INTERNET_DISCONNECTED")){
+            showBottomSheetDialogFragment();
+        }
+        if (errorCode == -10 || description.equals("net::ERR_UNKNOWN_URL_SCHEME")){
+            webView.setVisibility(View.GONE);
+            progress.setVisibility(View.VISIBLE);
+        }
+        Log.d(TAG, "onPageError: "+errorCode+"\n"+description+"\n"+failingUrl);
     }
 
     @Override
